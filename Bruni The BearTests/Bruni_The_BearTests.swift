28e076cb37b92d34af11c9001dac988d53c16b82
Lorenzo Franceschini <l.franceschini@gmail.com>
2016-04-27 09:08:59 +0200

    Initial Commit

---
+++ Bruni The BearTests/Bruni_The_BearTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bruni_The_BearTests.swift
+//  Bruni The BearTests
+//
+//  Created by Lorenzo on 27/04/16.
+//  Copyright © 2016 Lorenzo Franceschini. All rights reserved.
+//
+
+import XCTest
+@testable import Bruni_The_Bear
+
+class Bruni_The_BearTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
