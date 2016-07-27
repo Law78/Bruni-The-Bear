108faea670b06c8cc2870e3b732d805c383e522f
Lorenzo Franceschini <l.franceschini@gmail.com>
2016-07-27 09:15:10 +0200

    Creato il pannello Pause - Lezione 45

--- GameplayScene.swift
+++ GameplayScene.swift
@@ -27,6 +27,8 @@ class GameplayScene: SKScene {
     let minX = CGFloat(-152)
     let maxX = CGFloat(152)
     
+    private var pausePanel: SKSpriteNode?
+    
     override func didMoveToView(view: SKView) {
         initializeVariables()
         physicsWorld.gravity = CGVectorMake(0, -1.98)
@@ -43,14 +45,32 @@ class GameplayScene: SKScene {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
             let location = touch.locationInNode(self)
-            if location.x > center {
-                moveLeft = false
-                //player?.animatePlayer(moveLeft: moveLeft)
-            } else {
-                moveLeft = true
-                //player?.animatePlayer(moveLeft: moveLeft)
+            if self.scene?.paused == false {
+                if location.x > center {
+                    moveLeft = false
+                    //player?.animatePlayer(moveLeft: moveLeft)
+                } else {
+                    moveLeft = true
+                    //player?.animatePlayer(moveLeft: moveLeft)
+                }
+                player?.animatePlayer(moveLeft: moveLeft)
             }
-            player?.animatePlayer(moveLeft: moveLeft)
+            
+            if nodeAtPoint(location).name == "Pause" && self.scene?.paused == false {
+                self.scene?.paused = true
+                createPausePanel()
+            }
+            
+            if nodeAtPoint(location).name == "Resume" {
+                pausePanel?.removeFromParent()
+                self.scene?.paused = false
+            }
+            
+            if nodeAtPoint(location).name == "Quit" {
+                let scene = MainMenuScene(fileNamed: "MainMenuScene")
+                scene?.scaleMode = .AspectFill
+                self.view?.presentScene(scene!, transition: SKTransition.fadeWithColor(UIColor.blackColor(), duration: 1.0))
+            }
 
         }
         
@@ -107,7 +127,48 @@ class GameplayScene: SKScene {
         }
     }
     
-    // Aggiunte Lorenzo
+    // Funzione per creare il Pause Panel
+    func createPausePanel(){
+        
+        // Il pausePanel sarà in parent con la mainCamera
+        pausePanel = SKSpriteNode(imageNamed: "Pause Menu")
+        let resumeBtn = SKSpriteNode(imageNamed: "Resume Button")
+        let quitBtn = SKSpriteNode(imageNamed: "Quit Button 2")
+        
+        // Posiziono il pannello al centro
+        pausePanel?.anchorPoint = CGPoint(x:0.5, y:0.5)
+        // Voglio ingrandire un pochino il size del Pause Panel
+        pausePanel?.xScale = 1.6
+        pausePanel?.yScale = 1.6
+        pausePanel?.zPosition = 5
+        
+        // La posizione sarà relativa alla posizione della mainCamera
+        pausePanel?.position = CGPoint(x: (self.mainCamera?.frame.size.width)! / 2,
+            y: (self.mainCamera?.frame.size.height)! / 2 )
+        
+        // Setto il name dei miei pulsanti perchè mi servirà per il confronto con nodeAtPoint
+        resumeBtn.name = "Resume"
+        // Il pulsante dovrà avere una zPosition più alta del Pause Panel
+        resumeBtn.zPosition = 6
+        resumeBtn.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        resumeBtn.position = CGPoint(x: (pausePanel?.position.x)!, y: (pausePanel?.position.y)! + 25)
+        
+        quitBtn.name = "Quit"
+        quitBtn.zPosition = 6
+        quitBtn.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        quitBtn.position = CGPoint(x: (pausePanel?.position.x)!, y: (pausePanel?.position.y)! - 45)
+        
+        // Aggiungo i pulsanti al pausePanel
+        pausePanel?.addChild(resumeBtn)
+        pausePanel?.addChild(quitBtn)
+        
+        // Aggiungo il pausePanel come parent del mainCamera
+        self.mainCamera?.addChild(pausePanel!)
+        
+        
+    }
+    
+    // Aggiunte Lorenzo, una funzione diversa per muovere il background.
     
     func moveBackground(){
         bg1?.moveBackground()
